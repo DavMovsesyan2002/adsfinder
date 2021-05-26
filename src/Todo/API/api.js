3b158ab85e558246764f302ae3a2c216164d154a
Davo <david.movsesyaan2002@gmail.com>
2021-05-26 23:28:41 +0400

    FIX: remove build script

--- src/Todo/API/api.js
+++ src/Todo/API/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: `https://jsonplaceholder.typicode.com/`
+    baseURL: `https://jsonplaceholder.typicode.com/`,
+    timeout:0
   });
